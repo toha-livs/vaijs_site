500094f9770866663c65b84ec27bd5669b56a9ae
toha-livs <tohalivs>
2018-03-23 15:11:39 +0200

    strat

---
+++ vanja_site/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+
+
+# https://www.google.com/maps/@50.4310975,30.5190359,19.25z?hl=ru-RU
+# https://web.telegram.org/#/im?p=u405824122_14320248921627549975
+# https://web.telegram.org/#/im?p=u363917903_1070674077946741758
